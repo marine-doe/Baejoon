70a0a8b263c8147cfc41de919d11d609ffe4c120
marine-doe <109343692+marine-doe@users.noreply.github.com>
2023-02-21 21:22:15 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 108 ms, Memory: 12784 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -5,35 +5,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		// 크로아티아 알파벳
-//		String[] ca = { "c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z=" };
+		String[] ca = { "c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z=" };
 
-		char[] line = sc.next().toCharArray();
+		String line = sc.next();
 
 		int count = 0;
-		for (int i = 0; i < line.length - 1; i++) {
-			if (line[i] == 'c') {
-				if (line[i + 1] == '=' || line[i + 1] == '-') {
-					count++;
-				}
-			} else if (line[i] == 'd') {
-				if (line[i + 1] == '-') {
-					count++;
-				} else if (i < line.length - 2) {
-					if (line[i + 1] == 'z' && line[i + 2] == '=') {
-						count++;
-					}
-				}
-			} else if (line[i] == 'l' && line[i + 1] == 'j') {
-				count++;
-			} else if (line[i] == 'n' && line[i + 1] == 'j') {
-				count++;
-			} else if (line[i + 1] == '=' && line[i] == 's') {
-				count++;
-			} else if (line[i + 1] == '=' && line[i] == 'z') {
-				count++;
-			}
+		for (int i = 0; i < ca.length; i++) {
+			line = line.replace(ca[i], "+");
 		}
 
-		System.out.println(line.length - count);
+		System.out.println(line.length());
 	}
 }
