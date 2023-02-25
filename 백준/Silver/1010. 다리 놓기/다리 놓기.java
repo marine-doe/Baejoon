85f0da7fa3c227b63ec689a82dfed57052ea804c
marine-doe <109343692+marine-doe@users.noreply.github.com>
2023-02-25 17:36:02 +0900

    [Silver V] Title: 다리 놓기, Time: 84 ms, Memory: 11972 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+	public static void main(String[] args) throws Exception {
+		int t = Integer.parseInt(br.readLine());
+
+		for (int tc = 0; tc < t; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			double n = Integer.parseInt(st.nextToken());
+			double m = Integer.parseInt(st.nextToken());
+			if(n == m) {
+				bw.write(1 + "\n");
+				continue;
+			}
+			double result = 1;
+
+			for (int i = 0; i < n; i++) {
+				result *= m - i;
+				result /= 1 + i;
+			}
+
+			bw.write((int) result + "\n");
+		}
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
