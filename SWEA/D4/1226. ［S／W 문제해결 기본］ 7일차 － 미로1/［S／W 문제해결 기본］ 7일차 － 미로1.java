d8a5e1ff8a2d1e64a8aa83ae059e53267a68a270
marine-doe <109343692+marine-doe@users.noreply.github.com>
2023-03-28 13:45:51 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로1, Time: 110 ms, Memory: 18,656 KB -BaekjoonHub

--- SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.java
+++ SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.java
@@ -1,55 +1,42 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class Solution {
-	static int[][] map;
+	static char[][] map;
+	static boolean flag;
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		for (int tc = 1; tc <= 10; tc++) {
+		for (int tc = 0; tc < 10; tc++) {
+			map = new char[16][16];
 			int t = Integer.parseInt(br.readLine());
-			map = new int[16][16];
-			int x1 = 0, y1 = 0, x2 = 0, y2 = 0;
 			for (int i = 0; i < 16; i++) {
 				String line = br.readLine();
-				for (int j = 0; j < 16; j++) {
-					map[i][j] = Character.getNumericValue(line.charAt(j));
-					if (map[i][j] == 2) {
-						x1 = i;
-						y1 = j;
-						map[i][j] = 0;
-					}
-					if (map[i][j] == 3) {
-						x2 = i;
-						y2 = j;
-					}
-				}
+				map[i] = line.toCharArray();
 			}
-
-			dfs(x1, y1);
-
-			if (map[x2][y2] == 1) {
-				bw.write("#" + t + " " + "1\n");
+			flag = false;
+			dfs(1, 1);
+			if (flag) {
+				System.out.printf("#%d 1\n", t);
 			} else {
-				bw.write("#" + t + " " + "0\n");
+				System.out.printf("#%d 0\n", t);
 			}
 		}
-		bw.flush();
-		bw.close();
-		br.close();
 	}
 
 	private static void dfs(int i, int j) {
-		if (i < 0 || j < 0 || i >= 16 || j >= 16) {
+		if (map[i][j] == '3') {
+			flag = true;
 			return;
 		}
 
-		if (map[i][j] != 1) {
-			map[i][j] = 1;
+		if (map[i][j] == '1') {
+			return;
+		}
+
+		if (map[i][j] == '0' || map[i][j] == '2') {
+			map[i][j] = '1';
 			dfs(i + 1, j);
 			dfs(i - 1, j);
 			dfs(i, j + 1);
