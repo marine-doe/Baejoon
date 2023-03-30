2af1335f74af629f35d162477dd92813ac963c8b
marine-doe <109343692+marine-doe@users.noreply.github.com>
2023-03-30 20:51:31 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 905 ms, Memory: 97,972 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -1,82 +1,80 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Scanner;
 
 public class Solution {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	static int[] p;
+	static class Edge implements Comparable<Edge> {
+		int ed;
+		double w;
 
-	public static void main(String[] args) throws Exception {
-		int t = Integer.parseInt(br.readLine());
+		public Edge(int ed, double w) {
+			this.ed = ed;
+			this.w = w;
+		}
+
+		@Override
+		public int compareTo(Edge o) {
+			return Double.compare(this.w, o.w);
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
 		for (int tc = 1; tc < t + 1; tc++) {
-			int n = Integer.parseInt(br.readLine());
+			int n = sc.nextInt(); // 정점의 수
+			
 			int[][] island = new int[2][n];
-			double[][] cost = new double[n * (n - 1) / 2][3];
+			
 			for (int i = 0; i < 2; i++) { // 0은 x좌표, 1은 y좌표
-				StringTokenizer st = new StringTokenizer(br.readLine());
 				for (int j = 0; j < n; j++) {
-					island[i][j] = Integer.parseInt(st.nextToken());
+					island[i][j] = sc.nextInt();
 				}
 			}
-			double e = Double.parseDouble(br.readLine());
+			
+			double e = sc.nextDouble();
 
-			int idx = 0;
+			List<Edge>[] adj = new ArrayList[n];
+
+			for (int i = 0; i < n; i++) {
+				adj[i] = new ArrayList<>();
+			}
+
 			for (int i = 0; i < n - 1; i++) {
 				for (int j = i + 1; j < n; j++) {
-					double dist = Math.pow(island[0][i] - island[0][j], 2) + Math.pow(island[1][i] - island[1][j], 2);
-					cost[idx][0] = i;
-					cost[idx][1] = j;
-					cost[idx++][2] = e * dist;
+					double cost = e * (Math.pow(island[0][i] - island[0][j], 2)
+							+ Math.pow(island[1][i] - island[1][j], 2));
+					adj[i].add(new Edge(j, cost));
+					adj[j].add(new Edge(i, cost));
 				}
 			}
-			Arrays.sort(cost, new Comparator<double[]>() {
-				@Override
-				public int compare(double[] o1, double[] o2) {
-					return o1[2] - o2[2] < 0 ? -1 : 1;
+
+			boolean[] visited = new boolean[n];
+
+			PriorityQueue<Edge> pq = new PriorityQueue<>();
+
+			pq.addAll(adj[0]);
+			
+			visited[0] = true;
+
+			int pick = 1;
+			double ans = 0;
+
+			while (pick < n) {
+				Edge E = pq.poll();
+				if (visited[E.ed]) {
+					continue;
 				}
-			});
-
-			p = new int[n];
-			for (int i = 0; i < n; i++) {
-				p[i] = i;
-			}
-			int pick = 0;
-			double result = 0L;
-
-			for (int i = 0; i < n * (n - 1) / 2; i++) {
-				int px = findset((int) cost[i][0]);
-				int py = findset((int) cost[i][1]);
-
-				if (px != py) {
-					union(px, py);
-					result += cost[i][2];
-					pick++;
-				}
-
-				if (pick == (n - 1))
-					break;
+				
+				ans += E.w;
+				pq.addAll(adj[E.ed]);
+				visited[E.ed] = true;
+				pick++;
 			}
 
-			long answer = Math.round(result);
-			bw.write("#" + tc + " " + answer + "\n");
+			long result = Math.round(ans);
+			System.out.printf("#%d %d\n", tc, result);
 		}
-		bw.flush();
-		bw.close();
-		br.close();
-	}
-
-	private static void union(int x, int y) {
-		p[y] = x;
-	}
-
-	private static int findset(int x) {
-		if (x != p[x])
-			p[x] = findset(p[x]);
-		return p[x];
 	}
 }
