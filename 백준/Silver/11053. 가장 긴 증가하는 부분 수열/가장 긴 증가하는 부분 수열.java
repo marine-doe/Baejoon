23ea68ed3a4c6b3ab7b6a4d8f8a7eeb55a09f834
marine-doe <109343692+marine-doe@users.noreply.github.com>
2023-05-01 17:44:58 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 180 ms, Memory: 16680 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+
+		int[] lis = new int[n];
+		lis[0] = 1;
+
+		for (int i = 1; i < n; i++) {
+			int value = 0;
+			for (int j = 0; j < i; j++) {
+				if (arr[j] < arr[i]) {
+					if(value < lis[j]) {
+						value = lis[j];
+					}
+				}
+			}
+			lis[i] = value + 1;
+		}
+		
+		int result = 0;
+		for (int i : lis) {
+			result = Math.max(result, i);
+		}
+		
+		System.out.println(result);
+	}
+}
