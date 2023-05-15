f7ecbabea8aa39b80561e7784a78c45554e9685a
marine-doe <109343692+marine-doe@users.noreply.github.com>
2023-05-15 16:29:13 +0900

    [Silver I] Title: 트럭, Time: 208 ms, Memory: 18992 KB -BaekjoonHub

---
+++ 백준/Silver/13335. 트럭/트럭.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int bridge_length = sc.nextInt();
+		int weight = sc.nextInt();
+		int[] truck_weights = new int[n];
+		for (int i = 0; i < n; i++) {
+			truck_weights[i] = sc.nextInt();
+		}
+		System.out.println(solution(bridge_length, weight, truck_weights));
+	}
+	
+    public static int solution(int bridge_length, int weight, int[] truck_weights) {
+        int time = 1;
+        int sum = truck_weights[0];
+        int idx = 1;
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{truck_weights[0], 1});
+        
+        while(!q.isEmpty()){
+            time++;
+            for(int i = 0; i < q.size(); i++){
+                int[] cur = q.poll();
+                cur[1]++;
+                q.offer(cur);
+            }
+            if(q.peek()[1] == bridge_length + 1){
+                int[] cur = q.poll();
+                sum -= cur[0];
+            }
+            if(idx < truck_weights.length && sum + truck_weights[idx] <= weight){
+                sum += truck_weights[idx];
+                q.offer(new int[]{truck_weights[idx++], 1});
+            }
+        }
+        
+        return time;
+    }
+}
