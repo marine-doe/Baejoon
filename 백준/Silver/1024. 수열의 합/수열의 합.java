881590965041e41df4b4debce0d1c4a65c9d3a2b
marine-doe <109343692+marine-doe@users.noreply.github.com>
2023-11-07 22:13:51 +0900

    [Silver II] Title: 수열의 합, Time: 76 ms, Memory: 11532 KB -BaekjoonHub

--- 백준/Silver/1024. 수열의 합/수열의 합.java
+++ 백준/Silver/1024. 수열의 합/수열의 합.java
@@ -8,46 +8,37 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(st.nextToken()); // 10억
         int l = Integer.parseInt(st.nextToken()); // 2 <= l <= 100
 
-        boolean flag = true;
         for (int i = l; i < 101; i++) {
+            int mid = n / i;
             if (i % 2 == 0) {
-                int mid = n / i;
-                if (mid - i / 2 + 1 >= 0) {
-                    int sum = 0;
-                    for (int j = mid - i / 2 + 1; j <= mid + i / 2; j++) {
-                        sum += j;
-                    }
-                    if (sum == n) {
-                        for (int j = mid - i / 2 + 1; j <= mid + i / 2; j++) {
-                            System.out.print(j + " ");
+                int start = mid - i / 2 + 1;
+                int end = mid + i / 2;
+                if (start >= 0) {
+                    if (i * (start + end) / 2 == n) {
+                        for (int j = start; j <= end; j++) {
+                            sb.append(j + " ");
                         }
-                        flag = false;
                         break;
                     }
                 }
             } else {
-                int mid = n / i;
-                if (mid - i / 2 >= 0) {
-                    int sum = 0;
-                    for (int j = mid - i / 2; j <= mid + i / 2; j++) {
-                        sum += j;
-                    }
-                    if (sum == n) {
-                        for (int j = mid - i / 2; j <= mid + i / 2; j++) {
-                            System.out.print(j + " ");
+                int start = mid - i / 2;
+                int end = mid + i / 2;
+                if (start >= 0) {
+                    if (i * (start + end) / 2 == n) {
+                        for (int j = start; j <= end; j++) {
+                            sb.append(j + " ");
                         }
-                        flag = false;
                         break;
                     }
                 }
             }
         }
-        if (flag) {
-            System.out.println(-1);
-        }
+        System.out.println(sb.length() == 0 ? -1 : sb);
     }
 }
