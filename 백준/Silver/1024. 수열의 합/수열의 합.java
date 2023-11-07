a1c28dc62a54549cfc56a22934c09754e7c4754f
marine-doe <109343692+marine-doe@users.noreply.github.com>
2023-11-07 21:42:57 +0900

    [Silver II] Title: 수열의 합, Time: 80 ms, Memory: 11560 KB -BaekjoonHub

--- 백준/Silver/1024. 수열의 합/수열의 합.java
+++ 백준/Silver/1024. 수열의 합/수열의 합.java
@@ -12,43 +12,41 @@ public class Main {
         int n = Integer.parseInt(st.nextToken()); // 10억
         int l = Integer.parseInt(st.nextToken()); // 2 <= l <= 100
 
-        int s = 0;
-        int e = 0;
-        int sum = 0;
-        int dist = 1;
-        int min = INF;
-
-        int j = 0;
-        for (int i = 0; i < n / 2 + 2; i++, dist++) {
-            sum += i;
-            while (sum > n) {
-                sum -= j++;
-                dist--;
-            }
-            while (dist > 100) {
-                sum -= j++;
-                dist--;
-            }
-            if (sum == n && dist < 101 && dist >= l) {
-                if (min > dist) {
-                    s = j;
-                    e = i;
-                    min = dist;
+        boolean flag = true;
+        for (int i = l; i < 101; i++) {
+            if (i % 2 == 0) {
+                int mid = n / i;
+                if (mid - i / 2 + 1 >= 0) {
+                    int sum = 0;
+                    for (int j = mid - i / 2 + 1; j <= mid + i / 2; j++) {
+                        sum += j;
+                    }
+                    if (sum == n) {
+                        for (int j = mid - i / 2 + 1; j <= mid + i / 2; j++) {
+                            System.out.print(j + " ");
+                        }
+                        flag = false;
+                        break;
+                    }
+                }
+            } else {
+                int mid = n / i;
+                if (mid - i / 2 >= 0) {
+                    int sum = 0;
+                    for (int j = mid - i / 2; j <= mid + i / 2; j++) {
+                        sum += j;
+                    }
+                    if (sum == n) {
+                        for (int j = mid - i / 2; j <= mid + i / 2; j++) {
+                            System.out.print(j + " ");
+                        }
+                        flag = false;
+                        break;
+                    }
                 }
             }
         }
-
-        if (min != INF) {
-            if (s == 0 && min - 1 >= l) {
-                for (int i = s + 1; i < e + 1; i++) {
-                    System.out.print(i + " ");
-                }
-            }else {
-                for (int i = s; i < e + 1; i++) {
-                    System.out.print(i + " ");
-                }
-            }
-        } else {
+        if (flag) {
             System.out.println(-1);
         }
     }
