7bfd4337026ae96529082e6625658b213f47f40a
marine-doe <109343692+marine-doe@users.noreply.github.com>
2023-12-27 21:57:11 +0900

    [Silver III] Title: 주유소, Time: 372 ms, Memory: 41448 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -7,30 +7,31 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        int[] dist = new int[n - 1];
-        int[] cost = new int[n];
-        int total = 0;
+        long[] dist = new long[n - 1];
+        long[] cost = new long[n];
+        long total = 0;
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n - 1; i++) {
-            dist[i] = Integer.parseInt(st.nextToken());
+            dist[i] = Long.parseLong(st.nextToken());
             total += dist[i];
         }
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            cost[i] = Integer.parseInt(st.nextToken());
+            cost[i] = Long.parseLong(st.nextToken());
         }
 
-        int result = 0;
-
+        long result = 0;
+        long past = 0;
         int start = 0, end = 1;
         while (end < n - 1) {
+            past += dist[end - 1];
             if (cost[start] > cost[end]) { // 뒤에가 더 싸면
-                while (start < end) {
-                    result += cost[start] * dist[start];
-                    total -= dist[start++];
-                }
+                result += cost[start] * past;
+                total -= past;
+                past = 0;
+                start = end;
             }
             end++;
         }
