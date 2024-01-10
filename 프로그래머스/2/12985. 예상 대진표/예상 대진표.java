25c5836fa6dcd9a6e167a92e5791db541dae34a3
marine-doe <109343692+marine-doe@users.noreply.github.com>
2024-01-10 23:34:18 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -0,0 +1,27 @@
+class Solution
+{
+    public int solution(int n, int a, int b)
+    {
+        int answer = 1;
+        
+        while (true) {
+            if (a % 2 == 1) {
+                a++;
+            }
+
+            if (b % 2 == 1) {
+                b++;
+            }
+            
+            if (a == b) {
+                break;
+            }
+            
+            a /= 2;
+            b /= 2;
+            answer++;
+        }
+
+        return answer;
+    }
+}
