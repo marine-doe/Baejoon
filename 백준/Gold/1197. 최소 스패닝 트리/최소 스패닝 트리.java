9b95a87311151f3f8e785bf7d8cc29cce1276bcc
marine-doe <109343692+marine-doe@users.noreply.github.com>
2024-09-06 11:17:01 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 448 ms, Memory: 55744 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,58 +1,66 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static class Edge implements Comparable<Edge>{
-		int ed, w;
+    static class Node implements Comparable<Node> {
+        int x, w;
 
-		public Edge(int ed, int w) {
-			this.ed = ed;
-			this.w = w;
-		}
+        public Node(int x, int w) {
+            this.x = x;
+            this.w = w;
+        }
 
-		@Override
-		public int compareTo(Edge o) {
-			return Integer.compare(this.w, o.w);
-		}
-	}
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int v = sc.nextInt();
-		int e = sc.nextInt();
-		
-		List<Edge>[] adj = new ArrayList[v + 1];
-		for (int i = 0; i < v + 1; i++) {
-			adj[i] = new ArrayList<>();
-		}
-		
-		for (int i = 0; i < e; i++) {
-			int a = sc.nextInt();
-			int b = sc.nextInt();
-			int w = sc.nextInt();
-			adj[a].add(new Edge(b, w));
-			adj[b].add(new Edge(a, w));
-		}
-		
-		boolean[] visited = new boolean[v + 1];
-		PriorityQueue<Edge> pq = new PriorityQueue<>();
-		pq.addAll(adj[1]);
-		visited[1] = true;
-		int count = 1;
-		long result = 0;
-		
-		while(count < v) {
-			Edge cur = pq.poll();
-			if(!visited[cur.ed]) {
-				count++;
-				visited[cur.ed] = true;
-				pq.addAll(adj[cur.ed]);
-				result += cur.w;
-			}
-		}
-		
-		System.out.println(result);
-	}
+        @Override
+        public int compareTo(Node o) {
+            return this.w - o.w;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int v = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        boolean[] visited = new boolean[v + 1];
+
+        List<Node>[] adj = new ArrayList[v + 1];
+        for (int i = 1; i < v + 1; i++) {
+            adj[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < e; i++) {
+            st = new StringTokenizer(br.readLine());
+            int pre = Integer.parseInt(st.nextToken());
+            int aft = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            adj[pre].add(new Node(aft, w));
+            adj[aft].add(new Node(pre, w));
+        }
+
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.offer(new Node(1, 0));
+
+        int answer = 0;
+        int count = 0;
+
+        here:
+        while (count < v) {
+            Node cur = pq.poll();
+            int pre = cur.x;
+            int w = cur.w;
+
+            if (!visited[pre]) {
+                visited[pre] = true;
+                for (int i = 0; i < adj[pre].size(); i++) {
+                    if (!visited[adj[pre].get(i).x]) {
+                        pq.offer(adj[pre].get(i));
+                    }
+                }
+                count++;
+                answer += w;
+            }
+        }
+
+        System.out.println(answer);
+    }
 }
