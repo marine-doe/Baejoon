2f1baf748859bee08820a6bd72b21c5bd6e76b48
marine-doe <109343692+marine-doe@users.noreply.github.com>
2025-02-22 19:02:10 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 68 ms, Memory: 11772 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -3,9 +3,8 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n, result;
+    static int n;
     static int[][] map;
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -20,44 +19,70 @@ public class Main {
             }
         }
 
-        dfs(0, 1, 0);
-        System.out.println(result);
+        /*
+        0 : 가로
+        1 : 대각선
+        2 : 세로
+         */
+        int[][][] dp = new int[n][n][3];
+
+        dp[0][1][0] = 1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < 3; k++) {
+                    if (k == 0) { // 가로로 놓인 애
+                        if (check(i, j, 0)) {
+                            // 가로로 놓을게
+                            dp[i][j + 1][0] += dp[i][j][k];
+                        }
+                        if (check(i, j, 1)) {
+                            // 대각선으로 놓을게
+                            dp[i + 1][j + 1][1] += dp[i][j][k];
+                        }
+                    } else if (k == 1) { // 대각선으로 놓인 애
+                        if (check(i, j, 0)) {
+                            // 가로로 놓을게
+                            dp[i][j + 1][0] += dp[i][j][k];
+                        }
+                        if (check(i, j, 1)) {
+                            // 대각선으로 놓을게
+                            dp[i + 1][j + 1][1] += dp[i][j][k];
+                        }
+                        if (check(i, j, 2)) {
+                            // 세로로 놓을게
+                            dp[i + 1][j][2] += dp[i][j][k];
+                        }
+                    } else { // k == 2 , 세로로 놓인 애
+                        if (check(i, j, 2)) {
+                            // 세로로 놓을게
+                            dp[i + 1][j][2] += dp[i][j][k];
+                        }
+                        if (check(i, j, 1)) {
+                            // 대각선으로 놓을게
+                            dp[i + 1][j + 1][1] += dp[i][j][k];
+                        }
+                    }
+                }
+            }
+        }
+
+        System.out.println(dp[n - 1][n - 1][0] + dp[n - 1][n - 1][1] + dp[n - 1][n - 1][2]);
     }
 
-    private static void dfs(int x, int y, int dir) {
-        if (x == n - 1 && y == n - 1) {
-            result++;
-            return;
+    private static boolean check(int i, int j, int dir) {
+        if (dir == 0) {
+            if (j < n - 1 && map[i][j + 1] == 0) return true;
         }
 
-        switch (dir) {
-            case 0: // 가로
-                if (y + 1 < n && map[x][y + 1] == 0) {
-                    dfs(x, y + 1, 0);
-                    if (x + 1 < n && map[x + 1][y] == 0 && map[x + 1][y + 1] == 0) {
-                        dfs(x + 1, y + 1, 1);
-                    }
-                }
-                break;
-            case 1: // 대각선
-                if (x + 1 < n && y + 1 < n && map[x + 1][y + 1] == 0 && map[x + 1][y] == 0 && map[x][y + 1] == 0) {
-                    dfs(x + 1, y + 1, 1);
-                }
-                if (y + 1 < n && map[x][y + 1] == 0) {
-                    dfs(x, y + 1, 0);
-                }
-                if (x + 1 < n && map[x + 1][y] == 0) {
-                    dfs(x + 1, y, 2);
-                }
-                break;
-            case 2: // 세로
-                if (x + 1 < n && map[x + 1][y] == 0) {
-                    dfs(x + 1, y, 2);
-                    if (y + 1 < n && map[x][y + 1] == 0 && map[x + 1][y + 1] == 0) {
-                        dfs(x + 1, y + 1, 1);
-                    }
-                }
-                break;
+        if (dir == 1) {
+            if (i < n - 1 && j < n - 1 && map[i][j + 1] == 0 && map[i + 1][j] == 0 && map[i + 1][j + 1] == 0) return true;
         }
+
+        if (dir == 2) {
+            if (i < n - 1 && map[i + 1][j] == 0) return true;
+        }
+
+        return false;
     }
 }
