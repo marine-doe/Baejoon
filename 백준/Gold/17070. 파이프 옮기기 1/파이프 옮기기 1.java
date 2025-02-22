25511a113d6b8482ce691f7d785478195aeb4936
marine-doe <109343692+marine-doe@users.noreply.github.com>
2025-02-22 19:08:50 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 64 ms, Memory: 11804 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -3,14 +3,12 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n;
-    static int[][] map;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        n = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
-        map = new int[n][n];
+        int[][] map = new int[n + 1][n + 1];
 
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
@@ -24,7 +22,7 @@ public class Main {
         1 : 대각선
         2 : 세로
          */
-        int[][][] dp = new int[n][n][3];
+        int[][][] dp = new int[n + 1][n + 1][3];
 
         dp[0][1][0] = 1;
 
@@ -32,33 +30,33 @@ public class Main {
             for (int j = 0; j < n; j++) {
                 for (int k = 0; k < 3; k++) {
                     if (k == 0) { // 가로로 놓인 애
-                        if (check(i, j, 0)) {
+                        if (map[i][j + 1] == 0) {
                             // 가로로 놓을게
                             dp[i][j + 1][0] += dp[i][j][k];
                         }
-                        if (check(i, j, 1)) {
+                        if (map[i][j + 1] == 0 && map[i + 1][j] == 0 && map[i + 1][j + 1] == 0) {
                             // 대각선으로 놓을게
                             dp[i + 1][j + 1][1] += dp[i][j][k];
                         }
                     } else if (k == 1) { // 대각선으로 놓인 애
-                        if (check(i, j, 0)) {
+                        if (map[i][j + 1] == 0) {
                             // 가로로 놓을게
                             dp[i][j + 1][0] += dp[i][j][k];
                         }
-                        if (check(i, j, 1)) {
+                        if (map[i][j + 1] == 0 && map[i + 1][j] == 0 && map[i + 1][j + 1] == 0) {
                             // 대각선으로 놓을게
                             dp[i + 1][j + 1][1] += dp[i][j][k];
                         }
-                        if (check(i, j, 2)) {
+                        if (map[i + 1][j] == 0) {
                             // 세로로 놓을게
                             dp[i + 1][j][2] += dp[i][j][k];
                         }
                     } else { // k == 2 , 세로로 놓인 애
-                        if (check(i, j, 2)) {
+                        if (map[i + 1][j] == 0) {
                             // 세로로 놓을게
                             dp[i + 1][j][2] += dp[i][j][k];
                         }
-                        if (check(i, j, 1)) {
+                        if (map[i][j + 1] == 0 && map[i + 1][j] == 0 && map[i + 1][j + 1] == 0) {
                             // 대각선으로 놓을게
                             dp[i + 1][j + 1][1] += dp[i][j][k];
                         }
@@ -68,21 +66,5 @@ public class Main {
         }
 
         System.out.println(dp[n - 1][n - 1][0] + dp[n - 1][n - 1][1] + dp[n - 1][n - 1][2]);
-    }
-
-    private static boolean check(int i, int j, int dir) {
-        if (dir == 0) {
-            if (j < n - 1 && map[i][j + 1] == 0) return true;
-        }
-
-        if (dir == 1) {
-            if (i < n - 1 && j < n - 1 && map[i][j + 1] == 0 && map[i + 1][j] == 0 && map[i + 1][j + 1] == 0) return true;
-        }
-
-        if (dir == 2) {
-            if (i < n - 1 && map[i + 1][j] == 0) return true;
-        }
-
-        return false;
     }
 }
